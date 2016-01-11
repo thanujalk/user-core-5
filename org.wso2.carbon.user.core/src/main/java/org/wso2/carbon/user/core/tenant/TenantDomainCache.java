48be2288e4cae7590660f8e62a184f8f6819df73
omindu <omindu@wso2.com>
2016-01-11 23:12:54 +0530

    TenantDomainCache refactor.

--- org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/tenant/TenantDomainCache.java
+++ org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/tenant/TenantDomainCache.java
@@ -22,12 +22,12 @@ package org.wso2.carbon.user.core.tenant;
  * Date: Oct 1, 2010 Time: 2:36:37 PM
  */
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-import javax.cache.Cache;
-import javax.cache.CacheManager;
-import javax.cache.Caching;
+//import org.apache.commons.logging.Log;
+//import org.apache.commons.logging.LogFactory;
+//
+//import javax.cache.Cache;
+//import javax.cache.CacheManager;
+//import javax.cache.Caching;
 
 /**
  * This is the tenant cache.
@@ -36,7 +36,7 @@ class TenantDomainCache {
 
     public static final String TENANT_DOMAIN_CACHE_MANAGER = "TENANT_DOMAIN_CACHE_MANAGER";
     public static final String TENANT_DOMAIN_CACHE = "TENANT_DOMAIN_CACHE";
-    private static Log log = LogFactory.getLog(TenantDomainCache.class);
+//    private static Log log = LogFactory.getLog(TenantDomainCache.class);
     private static TenantDomainCache tenantDomainCache = new TenantDomainCache();
 
     private TenantDomainCache() {
@@ -51,11 +51,11 @@ class TenantDomainCache {
         return tenantDomainCache;
     }
 
-    private Cache<TenantIdKey, TenantDomainEntry> getTenantDomainCache() {
-        CacheManager cacheManager = Caching.getCacheManagerFactory().getCacheManager(TENANT_DOMAIN_CACHE_MANAGER);
-        Cache<TenantIdKey, TenantDomainEntry> cache = cacheManager.getCache(TENANT_DOMAIN_CACHE);
-        return cache;
-    }
+//    private Cache<TenantIdKey, TenantDomainEntry> getTenantDomainCache() {
+//        CacheManager cacheManager = Caching.getCacheManagerFactory().getCacheManager(TENANT_DOMAIN_CACHE_MANAGER);
+//        Cache<TenantIdKey, TenantDomainEntry> cache = cacheManager.getCache(TENANT_DOMAIN_CACHE);
+//        return cache;
+//    }
 
     /**
      * Add a cache entry.
@@ -67,16 +67,16 @@ class TenantDomainCache {
         // Element already in the cache. Remove it first
         clearCacheEntry(key);
 
-        Cache<TenantIdKey, TenantDomainEntry> cache = getTenantDomainCache();
-        if (cache != null) {
-            cache.put(key, entry);
-            log.debug(TENANT_DOMAIN_CACHE + " which is under " + TENANT_DOMAIN_CACHE_MANAGER + ", added the entry : " + entry
-                    + " for the key : " + key + " successfully");
-        } else {
-            if (log.isDebugEnabled()) {
-                log.debug("Error while getting the cache : " + TENANT_DOMAIN_CACHE + " which is under " + TENANT_DOMAIN_CACHE_MANAGER);
-            }
-        }
+//        Cache<TenantIdKey, TenantDomainEntry> cache = getTenantDomainCache();
+//        if (cache != null) {
+//            cache.put(key, entry);
+//            log.debug(TENANT_DOMAIN_CACHE + " which is under " + TENANT_DOMAIN_CACHE_MANAGER + ", added the entry : " + entry
+//                    + " for the key : " + key + " successfully");
+//        } else {
+//            if (log.isDebugEnabled()) {
+//                log.debug("Error while getting the cache : " + TENANT_DOMAIN_CACHE + " which is under " + TENANT_DOMAIN_CACHE_MANAGER);
+//            }
+//        }
     }
 
     /**
@@ -86,24 +86,24 @@ class TenantDomainCache {
      * @return Cached entry if the key presents, else returns null.
      */
     public TenantDomainEntry getValueFromCache(TenantIdKey key) {
-        Cache<TenantIdKey, TenantDomainEntry> cache = getTenantDomainCache();
-        if (cache != null) {
-            if (cache.containsKey(key)) {
-                TenantDomainEntry entry = cache.get(key);
-                if (log.isDebugEnabled()) {
-                    log.debug(TENANT_DOMAIN_CACHE + " which is under " + TENANT_DOMAIN_CACHE_MANAGER + ", found the entry : " + entry
-                            + " for the key : " + key + " successfully");
-                }
-                return entry;
-            }
-            if (log.isDebugEnabled()) {
-                log.debug(TENANT_DOMAIN_CACHE + " which is under " + TENANT_DOMAIN_CACHE_MANAGER + ", doesn't contain the key : " + key);
-            }
-        } else {
-            if (log.isDebugEnabled()) {
-                log.debug("Error while getting the cache : " + TENANT_DOMAIN_CACHE + " which is under " + TENANT_DOMAIN_CACHE_MANAGER);
-            }
-        }
+//        Cache<TenantIdKey, TenantDomainEntry> cache = getTenantDomainCache();
+//        if (cache != null) {
+//            if (cache.containsKey(key)) {
+//                TenantDomainEntry entry = cache.get(key);
+//                if (log.isDebugEnabled()) {
+//                    log.debug(TENANT_DOMAIN_CACHE + " which is under " + TENANT_DOMAIN_CACHE_MANAGER + ", found the entry : " + entry
+//                            + " for the key : " + key + " successfully");
+//                }
+//                return entry;
+//            }
+//            if (log.isDebugEnabled()) {
+//                log.debug(TENANT_DOMAIN_CACHE + " which is under " + TENANT_DOMAIN_CACHE_MANAGER + ", doesn't contain the key : " + key);
+//            }
+//        } else {
+//            if (log.isDebugEnabled()) {
+//                log.debug("Error while getting the cache : " + TENANT_DOMAIN_CACHE + " which is under " + TENANT_DOMAIN_CACHE_MANAGER);
+//            }
+//        }
         return null;
     }
 
@@ -114,40 +114,40 @@ class TenantDomainCache {
      * @param key Key to clear cache.
      */
     public void clearCacheEntry(TenantIdKey key) {
-        Cache<TenantIdKey, TenantDomainEntry> cache = getTenantDomainCache();
-        if (cache != null) {
-            if (cache.containsKey(key)) {
-                cache.remove(key);
-                if (log.isDebugEnabled()) {
-                    log.debug(TENANT_DOMAIN_CACHE + " which is under " + TENANT_DOMAIN_CACHE_MANAGER + ", is removed entry for the key : " + key + " successfully");
-                }
-            }
-
-            if (log.isDebugEnabled()) {
-                log.debug(TENANT_DOMAIN_CACHE + " which is under " + TENANT_DOMAIN_CACHE_MANAGER + ", doen't contain the key : " + key);
-            }
-        } else {
-            if (log.isDebugEnabled()) {
-                log.debug("Error while getting the cache : " + TENANT_DOMAIN_CACHE + " which is under " + TENANT_DOMAIN_CACHE_MANAGER);
-            }
-        }
+//        Cache<TenantIdKey, TenantDomainEntry> cache = getTenantDomainCache();
+//        if (cache != null) {
+//            if (cache.containsKey(key)) {
+//                cache.remove(key);
+//                if (log.isDebugEnabled()) {
+//                    log.debug(TENANT_DOMAIN_CACHE + " which is under " + TENANT_DOMAIN_CACHE_MANAGER + ", is removed entry for the key : " + key + " successfully");
+//                }
+//            }
+//
+//            if (log.isDebugEnabled()) {
+//                log.debug(TENANT_DOMAIN_CACHE + " which is under " + TENANT_DOMAIN_CACHE_MANAGER + ", doen't contain the key : " + key);
+//            }
+//        } else {
+//            if (log.isDebugEnabled()) {
+//                log.debug("Error while getting the cache : " + TENANT_DOMAIN_CACHE + " which is under " + TENANT_DOMAIN_CACHE_MANAGER);
+//            }
+//        }
     }
 
     /**
      * Remove everything in the cache.
      */
     public void clear() {
-        Cache<TenantIdKey, TenantDomainEntry> cache = getTenantDomainCache();
-        if (cache != null) {
-            cache.removeAll();
-            if (log.isDebugEnabled()) {
-                log.debug(TENANT_DOMAIN_CACHE + " which is under " + TENANT_DOMAIN_CACHE_MANAGER + ", is cleared successfully");
-            }
-        } else {
-            if (log.isDebugEnabled()) {
-                log.debug("Error while getting the cache : " + TENANT_DOMAIN_CACHE + " which is under " + TENANT_DOMAIN_CACHE_MANAGER);
-            }
-        }
+//        Cache<TenantIdKey, TenantDomainEntry> cache = getTenantDomainCache();
+//        if (cache != null) {
+//            cache.removeAll();
+//            if (log.isDebugEnabled()) {
+//                log.debug(TENANT_DOMAIN_CACHE + " which is under " + TENANT_DOMAIN_CACHE_MANAGER + ", is cleared successfully");
+//            }
+//        } else {
+//            if (log.isDebugEnabled()) {
+//                log.debug("Error while getting the cache : " + TENANT_DOMAIN_CACHE + " which is under " + TENANT_DOMAIN_CACHE_MANAGER);
+//            }
+//        }
     }
 
 }
