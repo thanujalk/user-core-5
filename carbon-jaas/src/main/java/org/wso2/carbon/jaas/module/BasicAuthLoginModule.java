adfe8a8a2f77edd7188cb3f9d93459502a9b14e7
omindu <omindu@wso2.com>
2016-01-12 18:38:29 +0530

    Adding LoginModule and CallbackHandler.

---
+++ carbon-jaas/src/main/java/org/wso2/carbon/jaas/module/BasicAuthLoginModule.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.carbon.jaas.module;
+
+import javax.security.auth.Subject;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.login.LoginException;
+import javax.security.auth.spi.LoginModule;
+import java.util.Map;
+
+
+/**
+ * This  LoginModule authenticates users with a password.
+ *
+ */
+public class BasicAuthLoginModule implements LoginModule {
+
+
+
+    @Override
+    public void initialize(Subject subject, CallbackHandler callbackHandler, Map<String, ?> sharedState,
+                           Map<String, ?> options) {
+
+    }
+
+    @Override
+    public boolean login() throws LoginException {
+
+
+
+
+        return false;
+    }
+
+    @Override
+    public boolean commit() throws LoginException {
+        return false;
+    }
+
+    @Override
+    public boolean abort() throws LoginException {
+        return false;
+    }
+
+    @Override
+    public boolean logout() throws LoginException {
+        return false;
+    }
+}
