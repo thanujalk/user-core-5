24f84b64791b2124fadce9b6727cb657f86605b1
thanujalk <jatljayasinghe@gmail.com>
2016-01-13 12:43:27 +0530

    Adding osgi related changes

---
+++ carbon-security/src/main/java/org/wso2/carbon/security/internal/CarbonSecurityProvider.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.carbon.security.internal;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.wso2.carbon.security.CarbonAuthenticator;
+import org.wso2.carbon.security.DefaultCarbonAuthenticator;
+
+/**
+ * OSGi services component which handle authentication and authorization
+ */
+@Component(
+        name = "org.wso2.carbon.security.internal.CarbonSecurityProvider",
+        immediate = true
+)
+public class CarbonSecurityProvider {
+
+    private ServiceRegistration<CarbonAuthenticator> serviceRegistration;
+
+    @Activate
+    public void registerCommandProvider(BundleContext bundleContext) {
+        serviceRegistration = bundleContext.registerService(CarbonAuthenticator.class,
+                                                            new DefaultCarbonAuthenticator(), null);
+    }
+
+    @Deactivate
+    public void unregisterCommandProvider(BundleContext bundleContext) {
+        if (serviceRegistration != null) {
+            serviceRegistration.unregister();
+        }
+    }
+
+}
+
